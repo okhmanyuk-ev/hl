9dddd8d4c042ed6d83e005c8c843048cad297269
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2020-04-06 13:01:45 +1000

    add mdl

---
+++ src/HL/mdlfile.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <common/bitbuffer.h>
+#include <common/buffer_helpers.h>
+#include <string>
+
+#include "studio.h"
+
+#include <vector>
+#include <platform/asset.h>
+
+class MDLFile
+{
+public:
+	void loadFromFile(const std::string& fileName)
+	{
+		auto asset = Platform::Asset::Asset(fileName);
+		mBuffer.write(asset.getMemory(), asset.getSize());
+	};
+
+public:
+	auto getHeader() { return (studiohdr_t*)mBuffer.getMemory(); }
+	
+private:
+	Common::BitBuffer mBuffer;
+};
