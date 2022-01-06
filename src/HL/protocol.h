59d9aa226d026e8749e4644d62ebf5424fb222bc
Evgeniy Okhmanyuk <okhmanyuk.ev@gmail.com>
2022-01-06 18:11:19 +1000

    wip

--- src/HL/protocol.h
+++ src/HL/protocol.h
@@ -568,3 +568,57 @@ namespace HL::Protocol
 		std::string sky_name;
 	};
 }
+
+#define BIT(n) (1<<(n))
+
+#define IN_ATTACK	(1<<0)
+#define IN_JUMP		(1<<1)
+#define IN_DUCK		(1<<2)
+#define IN_FORWARD	(1<<3)
+#define IN_BACK		(1<<4)
+#define IN_USE		(1<<5)
+#define IN_CANCEL	(1<<6)
+#define IN_LEFT		(1<<7)
+#define IN_RIGHT	(1<<8)
+#define IN_MOVELEFT	(1<<9)
+#define IN_MOVERIGHT	(1<<10)
+#define IN_ATTACK2	(1<<11)
+#define IN_RUN		(1<<12)
+#define IN_RELOAD	(1<<13)
+#define IN_ALT1		(1<<14)
+#define IN_SCORE	(1<<15)	// Used by client.dll for when scoreboard is held down
+
+// edict->flags
+#define FL_FLY                      BIT(0)  // Changes the SV_Movestep() behavior to not need to be on ground
+#define FL_SWIM                     BIT(1)  // Changes the SV_Movestep() behavior to not need to be on ground (but stay in water)
+#define FL_CONVEYOR                 BIT(2)
+#define FL_CLIENT                   BIT(3)
+#define FL_INWATER                  BIT(4)
+#define FL_MONSTER                  BIT(5)
+#define FL_GODMODE                  BIT(6)
+#define FL_NOTARGET                 BIT(7)
+#define FL_SKIPLOCALHOST            BIT(8)  // Don't send entity to local host, it's predicting this entity itself
+#define FL_ONGROUND                 BIT(9)  // At rest / on the ground
+#define FL_PARTIALGROUND            BIT(10) // not all corners are valid
+#define FL_WATERJUMP                BIT(11) // player jumping out of water
+#define FL_FROZEN                   BIT(12) // Player is frozen for 3rd person camera
+#define FL_FAKECLIENT               BIT(13) // JAC: fake client, simulated server side; don't send network messages to them
+#define FL_DUCKING                  BIT(14) // Player flag -- Player is fully crouched
+#define FL_FLOAT                    BIT(15) // Apply floating force to this entity when in water
+#define FL_GRAPHED                  BIT(16) // worldgraph has this ent listed as something that blocks a connection
+
+// UNDONE: Do we need these?
+#define FL_IMMUNE_WATER             BIT(17)
+#define FL_IMMUNE_SLIME             BIT(18)
+#define FL_IMMUNE_LAVA              BIT(19)
+
+#define FL_PROXY                    BIT(20) // This is a spectator proxy
+#define FL_ALWAYSTHINK              BIT(21) // Brush model flag -- call think every frame regardless of nextthink - ltime (for constantly changing velocity/path)
+#define FL_BASEVELOCITY             BIT(22) // Base velocity has been applied this frame (used to convert base velocity into momentum)
+#define FL_MONSTERCLIP              BIT(23) // Only collide in with monsters who have FL_MONSTERCLIP set
+#define FL_ONTRAIN                  BIT(24) // Player is _controlling_ a train, so movement commands should be ignored on client during prediction.
+#define FL_WORLDBRUSH               BIT(25) // Not moveable/removeable brush entity (really part of the world, but represented as an entity for transparency or something)
+#define FL_SPECTATOR                BIT(26) // This client is a spectator, don't run touch functions, etc.
+#define FL_CUSTOMENTITY             BIT(29) // This is a custom entity
+#define FL_KILLME                   BIT(30) // This entity is marked for death -- This allows the engine to kill ents at the appropriate time
+#define FL_DORMANT                  BIT(31) // Entity is dormant, no updates to client
